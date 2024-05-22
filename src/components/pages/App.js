f569f11cc928e60cc17266b872aacd1073ed389a
Bonilha-Arthur <arthurbonilhan@gmail.com>
2024-05-21 23:21:09 -0300

    Adicionando chamadas da api, ajustando alguns codigos

--- src/components/pages/App.js
+++ src/components/pages/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react'
-import { Container, Paper } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { Container, Paper, Button, Typography, Grid } from '@material-ui/core'
 import UsersList from '../organisms/UsersList'
 import SearchInput from '../molecules/SearchInput'
-import UserForm from '../organisms/UserForm'
+import api from '../utils/api'
+import Modal from '../molecules/Modal'
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
-  const [showForm, setShowForm] = useState(false)
+  const [showFormModal, setShowFormModal] = useState(false)
   const [selectedUser, setSelectedUser] = useState(null)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const fetchedUsers = await api.getUsers()
+      setUsers(fetchedUsers)
+    }
+    fetchUsers()
+  }, [])
 
   const handleSearchChange = (value) => {
     setSearchTerm(value)
@@ -15,28 +25,37 @@ const App = () => {
 
   const handleOpenForm = (user = null) => {
     setSelectedUser(user)
-    setShowForm(true)
+    setShowFormModal(true)
   }
 
-  const handleCloseForm = () => {
-    setShowForm(false)
+  const handleCloseFormModal = () => {
+    setShowFormModal(false)
     setSelectedUser(null)
   }
 
-  const handleUserSubmit = () => {
-    setShowForm(false)
-    setSelectedUser(null)
+  const handleUserSubmit = async (newUser) => {
+    await api.createUser(newUser)
+
+    const updatedUsers = await api.getUsers()
+    setUsers(updatedUsers.data)
+
+    handleCloseFormModal()
   }
 
   return (
     <Container maxWidth="md">
-      <Paper elevation={3}>
-        <h1>Lista de Usuários</h1>
+      <Paper elevation={3} style={{ padding: '20px' }}>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Typography variant="h5">Gerenciar Usuários</Typography>
+          <Button variant="contained" color="primary" onClick={() => handleOpenForm(null)}>
+            CADASTRAR
+          </Button>
+        </Grid>
         <SearchInput onChange={handleSearchChange} />
-        <UsersList searchTerm={searchTerm} onOpenForm={handleOpenForm} />
+        <UsersList users={users} searchTerm={searchTerm} onOpenForm={handleOpenForm} />
       </Paper>
 
-      {showForm && <UserForm user={selectedUser} onSubmit={handleUserSubmit} onClose={handleCloseForm} />}
+      <Modal open={showFormModal} onClose={handleCloseFormModal} onSubmit={handleUserSubmit} />
     </Container>
   )
 }
---
+++ src/components/utils/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/usuarios'
+
+const getUsers = async () => {
+  const response = await axios.get(baseURL)
+  return response.data
+}
+
+const createUser = async (user) => {
+  const response = await axios.post(baseURL, user)
+  return response.data
+}
+
+const updateUser = async (id, user) => {
+  const response = await axios.put(`${baseURL}/${id}`, user)
+  return response.data
+}
+
+const deleteUser = async (id) => {
+  await axios.delete(`${baseURL}/${id}`)
+}
+
+export default {
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+}
